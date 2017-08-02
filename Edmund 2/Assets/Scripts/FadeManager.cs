197f4d70ebfef8b19fdf2517aaedfcf10b0c39a0
PORTABLELO\Jonathan <pplorux@hotmail.com>
2017-08-02 15:58:20 +0200

    Remise en état FadeManager et WorldInteraction.

--- Edmund 2/Assets/Scripts/FadeManager.cs
+++ Edmund 2/Assets/Scripts/FadeManager.cs
@@ -7,15 +7,32 @@ public class FadeManager : MonoBehaviour
 {
     public Image fadePanel;
     public float fadeTime;
+    public enum EnumEtatFade {FadeIn, FadeOut};
+    public EnumEtatFade etatFade;
 
     private void Start()
     {
         fadePanel = GetComponent<Image>();
-        fadePanel.CrossFadeAlpha(0f, fadeTime, false);
+        //fadePanel.CrossFadeAlpha(0f, fadeTime, false);
+        switch (etatFade)
+        {
+            case EnumEtatFade.FadeIn:
+                FadeInMethod();
+                break;
+
+            case EnumEtatFade.FadeOut:
+                FadeOutMethod();
+                break;
+        }
     }
 
-    public void FadeOut()
+    public void FadeOutMethod()
     {
+        fadePanel.CrossFadeAlpha(0f, fadeTime, false);
+    }
+    public void FadeInMethod()
+    {
+        fadePanel.canvasRenderer.SetAlpha(0.0f);
         fadePanel.CrossFadeAlpha(1f, fadeTime, false);
     }
 }
--- Edmund 2/Assets/Scripts/WorldInteraction.cs
+++ Edmund 2/Assets/Scripts/WorldInteraction.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class WorldInteraction : MonoBehaviour
 {
     NavMeshAgent playerAgent;
+    Scene currentScene;
+    public AudioSource deathSfx;
+    public Image fadePanel;
 
     private void Start()
     {
         playerAgent = GetComponent<NavMeshAgent>();
+        currentScene = SceneManager.GetActiveScene();
     }
 
     private void Update()
@@ -39,4 +45,32 @@ public class WorldInteraction : MonoBehaviour
         }
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        //next scene trigger
+        if (other.tag == "Next Scene Trigger")
+            SceneManager.LoadScene(currentScene.buildIndex + 1);
+        //spikes trigger
+
+        //spikes or killbox
+        else if (other.tag == "Spikes Trigger")
+        {
+            StopsPlayer();
+            deathSfx.Play();
+            fadePanel.CrossFadeAlpha(1f, 3f, false);
+            StartCoroutine(WaitABit(3f));
+            
+        }
+    }
+
+    void StopsPlayer()
+    {
+        playerAgent.isStopped = true;
+    }
+
+    IEnumerator WaitABit (float timeInSeconds)
+    {
+        yield return new WaitForSeconds(timeInSeconds);
+        SceneManager.LoadScene(currentScene.buildIndex);
+    }
 }
